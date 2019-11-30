22396866123b755278bcbed02a483ab04e2fb7b2
Михаил О.Деньгин <denginm@gmail.com>
2019-11-30 20:23:05 +0300

    initial state

---
+++ ipam_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IpamApiConfig(AppConfig):
+    name = 'ipam_api'
--- pipam/settings.py
+++ pipam/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'ipam_api.apps.IpamApiConfig'
 ]
 
 MIDDLEWARE = [
